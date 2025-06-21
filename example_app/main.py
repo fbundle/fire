56ed59e15135499dcebd5cb1d6a26fc8da4ff3ec
khanh <khanh.nguyen.contact@gmail.com>
2025-06-21 08:30:42 +0800

    auto_push

--- example_app/main.py
+++ example_app/main.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     config_path, i = sys.argv[1], int(sys.argv[2])
     config = json.loads(open(config_path).read())[i]
     print(config, os.environ["NAME"], os.environ["AGE"], file=sys.stdout, flush=True)
-    sys.stdout.flush()
     for i in range(30):
         print(i, file=sys.stdout, flush=True)
-        sys.stdout.flush()
         time.sleep(1)
