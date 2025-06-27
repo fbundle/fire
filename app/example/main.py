f09fa0cca475f17fcbb8577b6a6295d2acaf0547
khanh <khanh.nguyen.contact@gmail.com>
2025-06-27 19:46:21 +0800

    auto_push

--- app/example/main.py
+++ app/example/main.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 if __name__ == "__main__":
-    config_path, i = sys.argv[1], int(sys.argv[2])
+    i, config_path = int(sys.argv[1]), sys.argv[2]
     config = json.loads(open(config_path).read())[i]
     print(config, os.environ["NAME"], os.environ["AGE"], file=sys.stdout, flush=True)
     for i in range(30):
