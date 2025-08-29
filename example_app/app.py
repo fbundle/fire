1bbae0baf77d8605fae51ccc8d4adc4e6c10ae19
khanh <khanh.nguyen.contact@gmail.com>
2025-08-29 16:49:18 +0800

    auto_push

--- example_app/app.py
+++ example_app/app.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import os
 import sys
@@ -7,6 +8,6 @@ if __name__ == "__main__":
     i, config_path = int(sys.argv[1]), sys.argv[2]
     config = json.loads(open(config_path).read())[i]
     print(i, config, os.environ["NAME"], os.environ["AGE"], file=sys.stdout, flush=True)
-    for i in range(30):
-        print(i, file=sys.stdout, flush=True)
+    for j in itertools.count():
+        print(j, file=sys.stdout, flush=True)
         time.sleep(1)
