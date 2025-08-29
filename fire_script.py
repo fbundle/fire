5f2de0540d3f0071def38b31211cb3de641e9b77
khanh <khanh.nguyen.contact@gmail.com>
2025-08-29 16:35:27 +0800

    auto_push

--- fire_script.py
+++ fire_script.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import json
+import uuid
 
 import fire
 
-TMP_DIR = "tmp"
+TMP_DIR = "tmp_deploy"
 
 script_header = """#!/usr/bin/env bash
 set -xe
@@ -11,50 +13,52 @@ set -xe
 
 if __name__ == '__main__':
     os.makedirs(TMP_DIR, exist_ok=True)
+
+    # config
+    config = [{
+        "name": "config_a"
+    }, {
+        "name": "config_b"
+    }]
+    config_name = "config.json"
+    config_path = f"{TMP_DIR}/{config_name}"
+    open(config_path, "w").write(json.dumps(config, indent=2))
+
+    # app
     app1 = fire.Process(
-        task_name="app_100_69_15_9",
+        task_name="example_app" + "_" + uuid.uuid4().hex,
         host_name="khanh@100.69.15.9",
         deploy_dir="/tmp",
         tmux_path="/opt/homebrew/bin/tmux",
     )
     app2 = fire.Process(
-        task_name="app_100_93_62_117",
+        task_name="example_app" + "_" + uuid.uuid4().hex,
         host_name="khanh@100.93.62.117",
         deploy_dir="/tmp",
         tmux_path="/usr/bin/tmux",
     )
 
-    # config
-    import json
+    ## clean
+    clean_script = ""
+    clean_script += app1.clean().export()
+    clean_script += app2.clean().export()
+    open(f"{TMP_DIR}/clean", "w").write(script_header + clean_script)
 
-    open(f"{TMP_DIR}/config.json", "w").write(json.dumps([
-        {
-            "name": "config_a"
-        },
-        {
-            "name": "config_b"
-        },
-    ]))
+    ## run
+    app_name = "app.py"
+    app_path = f"example_app/{app_name}"
 
-    # clean
-    clean = script_header
-    clean += app1.clean().export()
-    clean += app2.clean().export()
-    open(f"{TMP_DIR}/clean", "w").write(clean)
-
-    # run
-    run = script_header
-    run += app1.push(src="app/app.py").push(src=f"{TMP_DIR}/config.json").exec(
-        command="/Users/khanh/miniforge3/envs/test/bin/python app.py 0 config.json",
+    run_script = ""
+    run_script += app1.push(src=app_path).push(src=config_path).exec(
+        command=f"/Users/khanh/miniforge3/envs/test/bin/python {app_name} 0 {config_name}",
         env={"NAME": "khanh", "AGE": "20"},
     ).export()
-    run += app2.push(src="app/app.py").push(src=f"{TMP_DIR}/config.json").exec(
-        command="/home/khanh/miniforge3/envs/test/bin/python app.py 1 config.json",
+    run_script += app2.push(src=app_path).push(src=config_path).exec(
+        command=f"/home/khanh/miniforge3/envs/test/bin/python {app_name} 1 {config_name}",
         env={"NAME": "khanh", "AGE": "21"},
     ).export()
-    open(f"{TMP_DIR}/run", "w").write(run)
+    open(f"{TMP_DIR}/run", "w").write(script_header + run_script)
 
     if len(sys.argv) > 1:
         name = sys.argv[1]
         os.system(f"{TMP_DIR}/{name}")
-
